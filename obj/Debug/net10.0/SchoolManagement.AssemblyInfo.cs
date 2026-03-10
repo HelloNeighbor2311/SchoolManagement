907fc9c1b7f59130346b08cb7d2a93ead464f1cf
Nguyen Viet Dat <vietdat1132@gmail.com>
2026-03-10 10:44:49 +0700

    Modified some files

--- obj/Debug/net10.0/SchoolManagement.AssemblyInfo.cs
+++ obj/Debug/net10.0/SchoolManagement.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SchoolManagement")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a20032e1a690d9e54153a522aa0ef5c12dd03a9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+83d9e088c1dcdba4dfe8b7133e7c01ed7178e550")]
 [assembly: System.Reflection.AssemblyProductAttribute("SchoolManagement")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SchoolManagement")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
